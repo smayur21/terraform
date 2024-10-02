967fcec5ebb958ca2700668e56bec381e9ec8379
smayur21 <mayur.07.21@gmail.com>
2024-10-02 13:22:39 +0530

    Update ec2.tf
    EC2 instance details updated

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
-resource "aws_instance" "AWS_server" {
+resource "aws_instance" "AWS_server-1" {
         ami = var.ami
         instance_type = var.instance_type
                 tags = {
                 Name = "EC2TerraVM-1"
         }
 }
-resource "aws_instance" "AWS_server" {
+resource "aws_instance" "AWS_server-2" {
         ami = var.ami
         instance_type = var.instance_type
                 tags = {
